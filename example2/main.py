43349f0798d2c6f8d590263687737a3fe67d54ec
Skanda Akkihebbal Prasanna <91215660+Skanda-sap@users.noreply.github.com>
2023-11-14 00:25:12 -0500

    Updated main.py to make changes to window height and width

--- example2/main.py
+++ example2/main.py
@@ -4,44 +4,52 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
 
 class SayHello(App):
     def build(self):
-        #returns a window object with all it's widgets
+        # Get the screen size
+        screen_width, screen_height = Window.size
+        print('{0} and {1}'. format(screen_width, screen_height))
+
+        # Set minimum window size for desktop and mobile
+        Window.minimum_width = screen_width * 1.0
+        Window.minimum_height = screen_height * 1.0
+
+        # returns a window object with all its widgets
         self.window = GridLayout()
         self.window.cols = 1
-        self.window.size_hint = (0.6, 0.7)
-        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
+        self.window.size_hint = (None, None)  # Disable automatic size_hint
+        self.window.width = screen_width * 0.2  # Set an initial width
+        self.window.height = screen_height * 0.5  # Set an initial height
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
 
         # image widget
         self.window.add_widget(Image(source="logo.png"))
 
         # label widget
         self.greeting = Label(
-                        text= "What's your name?",
-                        font_size= 18,
-                        color= '#FFA500'
-                        )
+            text="What's your name?",
+            font_size=18,
+            color='#FFA500'
+        )
         self.window.add_widget(self.greeting)
 
         # text input widget
         self.user = TextInput(
-                    multiline= False,
-                    padding_y= (20,20),
-                    size_hint= (1, 0.5)
-                    )
+            multiline=False,
+            size_hint=(1, 0.5)
+        )
 
         self.window.add_widget(self.user)
 
         # button widget
         self.button = Button(
-                      text= "GREET",
-                      size_hint= (1,0.5),
-                      bold= True,
-                      background_color ='#00FFCE',
-                      #remove darker overlay of background colour
-                      # background_normal = ""
-                      )
+            text="GREET",
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#00FFCE',
+        )
         self.button.bind(on_press=self.callback)
         self.window.add_widget(self.button)
 
